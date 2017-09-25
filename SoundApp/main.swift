a6823d16f562dbb2d5705bd79b6b69b45e379b71
Olivia Hess <oliviahess@macbook-x.local>
2017-09-25 14:22:26 -0700

    Initial Commit

---
+++ SoundApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SoundApp
+//
+//  Created by Olivia Hess on 9/25/17.
+//  Copyright © 2017 Olivia Hess. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
